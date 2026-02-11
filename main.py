b3bfae92723d147b48cfaa3231753337cc6281f7
bcoconuts <bjburgess96@gmail.com>
2026-02-11 10:30:19 -0600

    Refactor password generation in main.py
    Refactor password generation logic to ensure all character types are included and simplify the process.

--- main.py
+++ main.py
@@ -4,32 +4,24 @@ import secrets
 import string
 import sys
 
-SET_LENGTH = int(sys.argv[1])//4
-SET_REMAINDER = int(sys.argv[1])%4
+LOWER_LETTER_SET = set(string.ascii_lowercase)
+UPPER_LETTER_SET = set(string.ascii_uppercase)
+DIGIT_SET = set(string.digits)
+PUNCTUATION_SET = set(string.punctuation)
+CHARACTERS = string.ascii_letters + string.digits + string.punctuation
 
 def main():
-    pre_password_set = set()
-    pre_password_set_lower = set()
-    pre_password_set_upper = set()
-    pre_password_set_digits = set()
-    pre_password_set_spchars = set()
-    pre_password_set_remainder = set()
-    while len(pre_password_set_lower) < SET_LENGTH:
-        pre_password_set_lower.add(secrets.choice(string.ascii_lowercase))
-    while len(pre_password_set_upper) < SET_LENGTH:
-        pre_password_set_upper.add(secrets.choice(string.ascii_uppercase))
-    while len(pre_password_set_digits) < SET_LENGTH:
-        pre_password_set_digits.add(secrets.choice(string.digits))
-    while len(pre_password_set_spchars) < SET_LENGTH:
-        pre_password_set_spchars.add(secrets.choice(string.punctuation))
-    for i in range(0, SET_REMAINDER):
-        pre_password_set_remainder.add(secrets.choice(string.ascii_letters))
-    pre_password_set.update(pre_password_set_spchars, pre_password_set_digits, pre_password_set_upper, pre_password_set_lower, pre_password_set_remainder)
-    post_password = ""
-    for i in pre_password_set:
-        post_password += i
-    print(post_password)
-    print(len(post_password))
+    if len(sys.argv) == 2:
+        set_length = int(sys.argv[1])
+    else:
+        set_length = 16
+    pass_set = set()
+    while len(pass_set) < set_length or pass_set.isdisjoint(LOWER_LETTER_SET) or pass_set.isdisjoint(UPPER_LETTER_SET) or pass_set.isdisjoint(DIGIT_SET) or pass_set.isdisjoint(PUNCTUATION_SET):
+        pass_set.add(secrets.choice(CHARACTERS))
+        if len(pass_set) > set_length:
+            pass_set.clear()
+    password = "".join(pass_set)
+    print(password, f"Password Length: {len(password)}")
 
 
 if __name__ == "__main__":
