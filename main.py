4625a1c20341dd2968316983eb11ba49a70230c5
bcoconuts <bjburgess96@gmail.com>
2026-02-11 18:30:39 -0600

    Refactor password generation to use list for characters
    Refactor password generation logic to use a list instead of a set for unique characters. Update variable names for clarity.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# password generator - Creates random passwor between 4-94 characters ling, with no repeated characters
+# password generator - Creates random passwor between 4-94 characters long, with no repeated characters. minimum of one lowercase, uppercase, digit, and sp char
 
 
 import secrets
@@ -21,9 +21,9 @@ CHARACTER_DICT = {
 DEFAULT_LENGTH = 16
 
 
-def check_length(set_length: int) -> int:
-    if 4 <= set_length <= 94:
-        return set_length
+def check_length(list_length: int) -> int:
+    if 4 <= list_length <= 94:
+        return list_length
     else:
         print(f"\nInput Out Of Range. Creating password of default length ({DEFAULT_LENGTH})")
         return DEFAULT_LENGTH
@@ -32,29 +32,35 @@ def check_length(set_length: int) -> int:
 def determine_length() -> int:
     argument_count = len(sys.argv)
     if argument_count == 1:
-        set_length = DEFAULT_LENGTH
+        list_length = DEFAULT_LENGTH
     elif argument_count == 2:
         try:
-            set_length = check_length(int(sys.argv[1]))
+            list_length = check_length(int(sys.argv[1]))
         except ValueError:
             print(f"\nInvalid Input. Creating password of default length ({DEFAULT_LENGTH})")
-            set_length = DEFAULT_LENGTH
+            list_length = DEFAULT_LENGTH
     else:
         print(f"\nToo Many Arguments Provided. Creating password of default length ({DEFAULT_LENGTH})")
-        set_length = DEFAULT_LENGTH
+        list_length = DEFAULT_LENGTH
 
-    return set_length
+    return list_length
 
 
 def main():
-    set_length = determine_length()
-    pass_set = {secrets.choice(CHARACTER_DICT[k]) for k in CHARACTER_DICT.keys() if not k == ALL_CHARS}
+    list_length = determine_length()
+    pass_list = [secrets.choice(CHARACTER_DICT[k]) for k in CHARACTER_DICT.keys() if not k == ALL_CHARS]
     available_chars = CHARACTER_DICT[ALL_CHARS]
-    while len(pass_set) < set_length:
+
+    for char in pass_list:
+        available_chars = available_chars.replace(char, "")
+
+    while len(pass_list) < list_length:
         next_char = secrets.choice(available_chars)
-        pass_set.add(next_char)
-        available_chars = available_chars[:available_chars.index(next_char)] + available_chars[available_chars.index(next_char) + 1:]
-    password = "".join(pass_set)
+        pass_list.append(next_char)
+        available_chars = available_chars.replace(next_char, "")
+        
+    secrets.SystemRandom().shuffle(pass_list)
+    password = "".join(pass_list)
     print(f"Password: {password}", f"\nPassword Length: {len(password)}")
 
 
